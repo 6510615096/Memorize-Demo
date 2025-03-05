4062995aac1fed5ff4eff04ed82d1983e1e24d17
Nutpupicha <6510615096@student.tu.ac.th>
2025-03-05 15:34:56 +0700

    done

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by นางสาวณัฐภูพิชา อรุณกรพสุรักษ์ on 5/3/2568 BE.
+//
+
+import Foundation
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -1,58 +1,38 @@
 //
-//  ContentView.swift
+//  EmojiMemoryGameView.swift
 //  Memorize
 //
 //  Created by นางสาวณัฐภูพิชา อรุณกรพสุรักษ์ on 19/2/2568 BE.
 //
 
+// View
 import SwiftUI
 
-struct ContentView: View {
-    var emojis = ["🎃","💀","🕸️","😈","🎃","💀","🕸️","😈"]
-    @State var cardCount = 4
+struct EmojiMemoryGameView: View {
+    var emojis = ["🎃","💀","🕸️","😈","👻","🙀","🍭","👹"]
     
     var body: some View {
         VStack {
             ScrollView {
                 cards
             }
-            cardCountAdjusters
         }
         .padding()
     }
     
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
-            ForEach(0..<cardCount, id: \.self) { index in
+            ForEach(emojis.indices, id: \.self) { index in
                 CardView(content: emojis[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.blue)
     }
-    
-    var cardCountAdjusters : some View {
-        HStack {
-            cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
-            Spacer()
-            cardCountAdjuster(by: 1, symbol: "rectangle.stack.fill.badge.plus")
-        }
-    }
-    
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button(action: {
-            cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-                .imageScale(.large)
-                .font(.largeTitle)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
 }
 
 #Preview {
-    ContentView()
+    EmojiMemoryGameView()
 }
 
 struct CardView: View {
